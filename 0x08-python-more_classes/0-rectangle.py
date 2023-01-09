1183fc9d40c08686734e856c02b5282f9df1585f
Gerson Antonio <gelsonantonio53@outlook.com>
2023-01-09 04:38:45 -0800

    tasks 0 - 9

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
-""" This is an empty class that defines what a rectangle is.
-A rectangle is a shape what has 4 sides and two of its sides
-are longer than the other 2"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-
-	"""This is a empty class that describes a rectangle"""
-	pass
+    """Represent a rectangle."""
+    pass
