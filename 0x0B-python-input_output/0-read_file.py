4d7be62efa4a0b8a4ce219ab81bb07a737cd559a
Gerson Antonio <gelsonantonio53@outlook.com>
2023-01-11 12:47:49 -0800

    tasks 0-9

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""
-Function that prints out the content of a file
-
-ARGS:
-filename - the name with the extension of the file to be read.
-reading - the variable holding the read method
-"""
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-	with open(filename, encoding="utf-8") as reading:
-		print(reading.read())
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""
-a function that writes a string to a file
-
-ARGS:
-filename - the name of the file to be created or updated
-text -  the string to be written to the file
-file_writter - theobject created
-"""
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-	with open(filename, mode="w", encoding="utf-8") as file_writter:
-		print(len(text))
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
