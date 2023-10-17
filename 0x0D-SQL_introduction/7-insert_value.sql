ac4f9c9df1587905708b6386000ec91744964844
Gerson Antonio <gelsonantonio53@outlook.com>
2023-10-17 23:58:36 +0200

    completed the implementation of the script that inserts a row into the table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,2 @@
 -- script that inserts a new row in the table first_table.
-INSERT INTO first_table VALUES (
-    id = 89,
-    name = "Best School"
-);
+INSERT INTO first_table VALUES(89, "Best School"));
