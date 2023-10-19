60cfd5447295f279217693299c455d7f1e445e9a
Gerson Antonio <gelsonantonio53@outlook.com>
2023-10-19 23:07:09 +0200

    implementation of 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,10 +5,12 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
--- then we insert the data into teh table
-INSERT INTO second_table VALUES(
-    (id = 1, name = “John”, score = 10),
-    (id = 2, name = “Alex”, score = 3),
-    (id = 3, name = “Bob”, score = 14),
-    (id = 4, name = “George”, score = 8)
-);
+-- then we insert the data into the table
+INSERT INTO second_table (id, name, score) VALUES
+    (id = 1, name = “John”, score = 10);
+INSERT INTO second_table (id, name, score) VALUES
+    (id = 2, name = “Alex”, score = 3);
+INSERT INTO second_table (id, name, score) VALUES
+    (id = 3, name = “Bob”, score = 14);
+INSERT INTO second_table (id, name, score) VALUES
+    (id = 4, name = “George”, score = 8);
