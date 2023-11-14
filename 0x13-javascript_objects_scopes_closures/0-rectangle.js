925b9863bde124e40ee389b9800b466f30e26fca
Gerson Antonio <gelsonantonio53@outlook.com>
2023-11-14 22:58:38 +0200

    corrected task 0 and 1

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 
 // empty class Rectangle that defines a rectangle.
-modules.exports = class Rectangle {
-    
-}
+module.exports = class Rectangle { };
--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -5,9 +5,9 @@
 // Initialize the instance attribute width with the value of w.
 // Initialize the instance attribute height with the value of h.
 
-modules.exports = class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
-}
+};
