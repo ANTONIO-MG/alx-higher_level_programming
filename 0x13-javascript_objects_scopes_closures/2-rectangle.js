54244f174895d6b5e4b355be9cd972a04923cf88
Gerson Antonio <gelsonantonio53@outlook.com>
2023-11-14 23:21:49 +0200

    completed task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -9,8 +9,7 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) {
-      this.width = undefined;
-      this.height = undefined;
+
     } else {
       this.width = w;
       this.height = h;
