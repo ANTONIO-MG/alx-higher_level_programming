be6b69f3dac3109457ba621fe1188d80cb535a56
Gerson Antonio <gelsonantonio53@outlook.com>
2023-12-16 12:39:56 -0800

    completed task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,19 @@
 #  script that lists all states from the database hbtn_0e_0_usa
 
 import sys
-import Mysqldb
-
-from SQLalchemy import connect, 
+import mysql.connector
 
 if __name__ == '__main__':
-    # imports the database and then makes a quere on the same database that has been imported
-    connection MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
-    
+    # imports the database and then makes a query
+    connection = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2]
+    )
+    mycursor = connection.cursor()
+    mycursor.execute("use hbtn_0e_0_usa")
+    mycursor.execute("select * from states")
+
+    for items in mycursor:
+        print(items)
