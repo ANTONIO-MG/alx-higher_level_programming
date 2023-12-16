a9d637ce85e7faff26dfecab2f81b58b238fe94f
Gerson Antonio <gelsonantonio53@outlook.com>
2023-12-16 12:56:12 -0800

    completed task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,18 +2,20 @@
 #  script that lists all states from the database hbtn_0e_0_usa
 
 import sys
-import mysql.connector
+import MySQLdb
 
 if __name__ == '__main__':
     # imports the database and then makes a query
-    connection = mysql.connector.connect(
+    connection = MySQLdb.connect(
             host="localhost",
             port=3306,
             user=sys.argv[1],
-            passwd=sys.argv[2]
+            passwd=sys.argv[2],
+            db=sys.argv[3]
     )
+
     mycursor = connection.cursor()
-    mycursor.execute("use hbtn_0e_0_usa")
+
     mycursor.execute("select * from states")
 
     for items in mycursor:
