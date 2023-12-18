623ae89a500ca703d86197885f92ce4884a397eb
Gerson Antonio <gelsonantonio53@outlook.com>
2023-12-18 09:33:34 -0800

    made corrections to task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
 """
-script that lists all cities from the database hbtn_0e_4_usa
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
 """
 
-import sys
 import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
     """
-    imports the database and then makes a query
+    Access to the database and get the cities
+    from the database.
     """
-    connection = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-    )
 
-    mycursor = connection.cursor()
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    mycursor.execute("select * from cities order by id asc")
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            ORDER BY
+                cities.id ASC
+        """)
 
-    for items in mycursor:
-        print(items)
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
